a0a6b89229364fae194d9c8acf53d193108dd1df
ItzelDiego <usersdiego38@gmail.com>
2025-03-12 10:45:46 -0500

    ProyecyoArudinos

---
+++ ProyectosArduino/js/imagen.js
@@ -0,0 +1,16 @@
+function mostrar() {
+    var image = document.getElementById("miImagen"); 
+  
+      image.style.display = "block"; 
+   
+  }
+  
+
+  function ocultar() {
+    var image = document.getElementById("miImagen"); 
+
+
+        image.style.display = "none"; 
+    
+    
+  }
